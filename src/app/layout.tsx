c582754b3c92b2a644bad8cdc72edbdb0119397f
Zellux Wang <zellux@gmail.com>
2026-07-02 03:05:34 -0700

    refactor(css): extract shared UI primitives

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ import "./reader-shell.css";
 import "./job-status.css";
 import "./settings.css";
 import "./source-dialog.css";
+import "./shared-ui.css";
 import "./globals.css";
 import "./public-pages.css";
 
